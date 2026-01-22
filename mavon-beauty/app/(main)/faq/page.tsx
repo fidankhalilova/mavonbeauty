be452837a07d4496abe7fadeb50920a28796a891
Aydan Abbaszadə <aydanaab@code.edu.az>
2026-01-22 23:24:47 +0400

    Complete FAQ page layout

---
+++ mavon-beauty/app/(main)/faq/page.tsx
@@ -0,0 +1,14 @@
+import SubsEmail from "@/Sections/Home/SubsEmail";
+import FaqPage from "./view";
+
+export default function Faq() {
+    return (
+        <main>
+            <FaqPage />
+            <div className="-mt-17.5">
+                <SubsEmail />
+            </div>
+
+        </main>
+    );
+}
